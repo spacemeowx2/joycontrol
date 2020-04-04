0430f0643e2a4ba7a8a964d9c1fb8ec6ccc5355a
spacemeowx2 <spacemeowx2@gmail.com>
2020-04-04 22:24:37 +0800

    add reconnect

--- run_amiibo_cli.py
+++ run_amiibo_cli.py
@@ -13,9 +13,6 @@ from joycontrol.server import create_hid_server
 logger = logging.getLogger(__name__)
 
 async def test_controller_buttons(controller_state: ControllerState):
-    """
-    Navigates to the "Test Controller Buttons" menu and presses all buttons
-    """
     await controller_state.connect()
 
     if False:
@@ -68,9 +65,12 @@ async def test_controller_buttons(controller_state: ControllerState):
     await cli.run()
 
 
-async def _main(controller, capture_file=None, spi_flash=None):
+async def _main(controller, reconnect_bt_addr=None, capture_file=None, spi_flash=None, device_id=None):
     factory = controller_protocol_factory(controller, spi_flash=spi_flash)
-    transport, protocol = await create_hid_server(factory, 17, 19, capture_file=capture_file)
+    ctl_psm, itr_psm = 17, 19    
+    transport, protocol = await create_hid_server(factory, 
+        reconnect_bt_addr=reconnect_bt_addr,
+        ctl_psm=ctl_psm, itr_psm=itr_psm, capture_file=capture_file, device_id=device_id)
 
     await test_controller_buttons(protocol.get_controller_state())
 
@@ -89,7 +89,10 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     #parser.add_argument('controller', help='JOYCON_R, JOYCON_L or PRO_CONTROLLER')
     parser.add_argument('-l', '--log')
+    parser.add_argument('-d', '--device_id')
     parser.add_argument('--spi_flash')
+    parser.add_argument('-r', '--reconnect_bt_addr', type=str, default=None, 
+        help='The Switch console bluetooth address, for reconnecting as an already paired controller')
     args = parser.parse_args()
 
     """
@@ -124,4 +127,10 @@ if __name__ == '__main__':
 
     with get_output(args.log) as capture_file:
         loop = asyncio.get_event_loop()
-        loop.run_until_complete(_main(controller, capture_file=capture_file, spi_flash=spi_flash))
+        loop.run_until_complete(_main(
+            controller,
+            reconnect_bt_addr=args.reconnect_bt_addr,
+            capture_file=capture_file,
+            spi_flash=spi_flash,
+            device_id=args.device_id
+        ))
